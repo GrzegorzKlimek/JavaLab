45209d8951c86d7dd720323c12abac44f78f1c96
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2018-01-11 13:52:38 +0100

    Add L11_finance project

---
+++ L11_Adv_finance/src/utilies/CSVreader.java
@@ -0,0 +1,25 @@
+package utilies;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVreader {
+
+	
+	public String []  read (String pathToCSVFile) throws IOException {	
+	        String line;
+	        String cvsSplitBy = ",";
+	        BufferedReader bufferReader = new BufferedReader(new FileReader(pathToCSVFile)); 
+			line = bufferReader.readLine();		
+			return line.split(cvsSplitBy);
+	}
+	     
+
+
+
+}
