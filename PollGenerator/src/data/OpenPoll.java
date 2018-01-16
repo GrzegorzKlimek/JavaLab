a3ddea046cd81d6076d4a372ad1419f389cd1168
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2018-01-16 22:08:15 +0100

    PollGenerator: user can list particular poll by it's id

--- PollGenerator/src/data/OpenPoll.java
+++ PollGenerator/src/data/OpenPoll.java
@@ -3,16 +3,31 @@ package data;
 import java.util.List;
 
 public class OpenPoll extends Poll {
-	List <String> answers;
+    List<String> answers;
 
-	public List<String> getAnswers() {
-		return answers;
-	}
+    public List<String> getAnswers() {
+        return answers;
+    }
 
-	public void setAnswers(List<String> answers) {
-		this.answers = answers;
-	}
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
 
+    @Override
+    public String toString() {
 
+        StringBuilder stringBuilder = new StringBuilder();
+        String newLine = System.lineSeparator();
+
+        stringBuilder.append(super.toString());
+
+        if (answers != null) {
+
+            for (String answer : answers) {
+                stringBuilder.append(answer).append(newLine);
+            }
+        }
+        return stringBuilder.toString();
+    }
 
 }
--- PollGenerator/src/view/PollLister.java
+++ PollGenerator/src/view/PollLister.java
@@ -23,7 +23,7 @@ public class PollLister {
         String command = commands[0];
         switch (command) {
             case "poll":
-
+                listPoll(ViewUtilis.tail(commands));
                 break;
             case "polls":
                 listPolls();
@@ -38,5 +38,32 @@ public class PollLister {
         for (Poll poll : polls) {
             System.out.println(poll);
         }
+    }
+
+    private void listPoll (String[] commands) {
+        if (commands.length < 2) {
+            System.out.println(ViewUtilis.WRONG_NUMBER_OF_ARGUMENTS);
+            return;
+        }
+
+        String comand = commands[0];
+
+        if (!comand.equals("id")) {
+            System.out.println(ViewUtilis.WRONG_COMMANT_MESSAGE);
+            return;
+        }
+
+        try {
+        int id =Integer.parseInt(commands[1]);
+
+        Poll poll = controller.getPolls().get(id -1);
+        System.out.println(poll);
+        } catch (NumberFormatException exeption) {
+            System.out.println("insert number as id!");
+        } catch (IndexOutOfBoundsException exeption) {
+            System.out.println("There are no poll with such id!");
+        }
+
+
     }
 }
